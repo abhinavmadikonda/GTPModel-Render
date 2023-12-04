3935a01b2c0b76369384d626bbf9bba1ca3af1c5
amadikon <amadikon@cisco.com>
2023-12-04 22:10:18 +0530

    custom gpt

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import openai
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/queryCustomGPT')
 def hello_world():
-    return 'Hello, World!'
+    openai.api_key = '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'
+    fine_tuned_model = "davinci:ft-personal-2023-12-04-10-36-00"
+    new_prompt = "Is it possible to book a commercial gas cylinder?"
+    answer = openai.Completion.create(
+        model=fine_tuned_model,
+        prompt=new_prompt,
+        max_tokens=100,
+        temperature=0
+    )
+    print(answer['choices'][0]['text'])
+    return answer['choices'][0]['text']
+
